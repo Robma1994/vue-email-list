74de5d86ee0db245c6c5dec8c371136a737c4afd
Roberto <robertomartinonwg@gmail.com>
2021-09-16 18:41:01 +0200

    Bonus completato

--- main.js
+++ main.js
@@ -1,13 +1,32 @@
-/*Attraverso l'apposita API di Boolean
-(https://flynn.boolean.careers/exercises/api/random/mail)
-generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
-*/
+//BONUS
 Vue.config.devtools = true; 
 const app = new Vue ({
     el: "#root",
     data: {
-        emails: []
+        emails: [],
+        arrivati: false
     },
+    mounted() {
+        let numeri = 10
+        for(let i = 0; i <= numeri - 1; i++) {
+            axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then((results) => {
+                    this.emails.push(results.data.response)
+                    if(this.emails.length < 10) {
+                        this.arrivati = false
+                    } else if (this.emails.length == 10) {
+                        this.arrivati = true
+                    }
+                });
+        }
+    },
+    
+/*Attraverso l'apposita API di Boolean
+(https://flynn.boolean.careers/exercises/api/random/mail)
+generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+*/
+
+    /*
     mounted() {
         let numeri = 10
         for(let i = 0; i <= numeri - 1; i++){
@@ -17,6 +36,7 @@ const app = new Vue ({
                 });
         }
     }
+    */
 })
 
 
